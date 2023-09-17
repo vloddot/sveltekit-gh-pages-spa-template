4e4df002e477d3b999225fe9a12d78ce93386717
vloddot <tamoraa08@gmail.com>
2023-09-17 12:52:59 +0300

    Updates

---
+++ _app/immutable/chunks/paths.b6a3f4e9.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_13tiorh)==null?void 0:s.base)??"/sveltekit-gh-pages-spa-template";var t;const a=((t=globalThis.__sveltekit_13tiorh)==null?void 0:t.assets)??e;export{a,e as b};
