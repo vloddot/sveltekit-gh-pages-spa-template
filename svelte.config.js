ae092e0929365d3d7ce0bd20277be0ddd0257a88
vloddot <tamoraa08@gmail.com>
2023-09-17 16:27:32 +0300

    test fallback only set to 404.html method

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		files: {
-			appTemplate: 'src/404.html'
-		},
 		paths: {
 			base: process.env.NODE_ENV == 'production' ? '/sveltekit-gh-pages-spa-template' : ''
 		},
+
 		adapter: adapter({ fallback: '404.html' })
 	}
 };
